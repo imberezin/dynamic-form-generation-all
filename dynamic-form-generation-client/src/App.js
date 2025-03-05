81a398faa457e5bb5043e5a7f14fea791755d88a
imberezin1 <imberezin@gmail.com>
2025-03-05 12:16:40 +0200

    clean App file

--- dynamic-form-generation-client/src/App.js
+++ dynamic-form-generation-client/src/App.js
@@ -11,16 +11,7 @@
  * - Coordinates schema updates between components
  */
 
-import React, { useReducer } from "react";
-// import { ThemeProvider, createTheme } from "@mui/material/styles";
-// import CssBaseline from "@mui/material/CssBaseline";
-// import { Container, Typography, Box, CircularProgress } from "@mui/material";
-// import DynamicForm from "./components/DynamicForm";
-// import SchemaUploader from "./components/SchemaUploader";
-import useFetch from "./hooks/useFetch";
-
-// Import reducer, initial state, and action creators
-import appReducer, { initialState, appActions } from "./reducers/appReducer";
+import React from "react";
 import MainView from "./components/MainView";
 import { FromProvider } from "./hooks/FromProvider";
 
@@ -28,47 +19,6 @@ import { FromProvider } from "./hooks/FromProvider";
 export const FromContext = React.createContext();
 
 function App() {
-  // const [state, dispatch] = useReducer(appReducer, initialState);
-  // const { fetchOptions } = state;
-
-  // const { formData, loading, error, setOptions } = useFetch(fetchOptions);
-
-  // function handleSchemaUpdate(timestamp) {
-  //   const newOptions = {
-  //     service: "schema",
-  //     endpoint: "getActive",
-  //     params: { timestamp },
-  //   };
-
-  //   // Use action creator to dispatch UPDATE_FETCH_OPTIONS action
-  //   dispatch(appActions.updateFetchOptions(newOptions));
-
-  //   // Also update the useFetch hook
-  //   setOptions(newOptions);
-  // }
-
-  // const initialOptions = {
-  //   service: "submission",
-  //   endpoint: "getAll",
-  //   params: {},
-  // };
-
-  // const {
-  //   formData: submissions = [],
-  //   submissionsLoading,
-  //   setOptions: submissionsSetOptions,
-  // } = useFetch(initialOptions);
-
-  // const updateSubmissionsList = () => {
-  //   console.log("Updating submissions list...");
-  //   const timestamp = new Date().toLocaleString().replace(",", "");
-  //   submissionsSetOptions({
-  //     service: "submission",
-  //     endpoint: "getAll",
-  //     params: { timestamp },
-  //   });
-  // };
-
   return (
     <FromProvider>
       <MainView />
